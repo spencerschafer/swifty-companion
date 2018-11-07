9379061e1af34d3cf474e1a01ba36fd6ac072f84
spencerschafer <spencerschafer@gmail.com>
2018-11-07 14:30:04 +0200

    Add pods to application

---
+++ src/SAS-SWIFTY/Pods/Target Support Files/Pods-SAS-SWIFTY/Pods-SAS-SWIFTY-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SAS_SWIFTY : NSObject
+@end
+@implementation PodsDummy_Pods_SAS_SWIFTY
+@end
---
+++ src/SAS-SWIFTY/Pods/Target Support Files/Pods-SAS-SWIFTYUITests/Pods-SAS-SWIFTYUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SAS_SWIFTYUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SAS_SWIFTYUITests
+@end
