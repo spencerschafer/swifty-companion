9d0419665d4cb5805dcb48547b626cccbf495df7
spencerschafer <spencerschafer@gmail.com>
2018-11-10 15:42:58 +0200

    Add Swifty

---
+++ src/Swifty/Swifty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty
+//
+//  Created by Spencer SCHAFER on 2018/11/10.
+//  Copyright © 2018 Spencer SCHAFER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
